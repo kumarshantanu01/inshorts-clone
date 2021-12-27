fe1e7e002e9de4a330b72b4ced6afe82d32bd0e8
kumarshantanu01 <shantanuk280@gmail.com>
2021-12-27 16:10:34 +0530

    feat: adding info header

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
-import {Box} from '@material-ui/core';
-import Header from './components/Header';
+import { Box, makeStyles } from "@material-ui/core";
+import Header from "./components/Header";
+import Infoheader from "./components/Infoheader";
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    marginTop: 110,
+    width: '59%',
+    margin: '0 auto',
+    [theme.breakpoints.down('md')]: {
+      width: '75%'
+    },
+    [theme.breakpoints.down('sm')]: {
+      width: '85%'
+    }
+  }
+}))
 
 function App() {
+  const classes = useStyles();
+
   return (
     <Box>
-      <Header/>
+      <Header />
+      <Box className={classes.container}>
+        <Infoheader />
+      </Box>
     </Box>
   );
 }
