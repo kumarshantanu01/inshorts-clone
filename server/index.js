84b921d512bcc891a4e8c95bb39f92b5174892ef
kumarshantanu01 <shantanuk280@gmail.com>
2022-01-17 14:31:48 +0530

    feat: connectiong to mongodb

--- server/index.js
+++ server/index.js
@@ -1,2 +1,11 @@
-import { Express } from "express";
+import express from "express";
 
+import Connection from "./connection/db.js";
+
+const app = express();
+
+const PORT = 8000;
+
+Connection();
+
+app.listen(PORT, () => console.log(`server is running on ${PORT}`));
