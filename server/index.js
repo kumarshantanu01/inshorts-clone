16bcabecc67cbe4af33218f69bdc66f79ca7dae0
kumarshantanu01 <shantanuk280@gmail.com>
2022-01-18 17:28:37 +0530

    feat: fixing cors error

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import cors from 'cors';
+import bodyParser from "body-parser";
 
 import Connection from "./connection/db.js";
 import DefaultData from "./default.js";
@@ -6,6 +8,9 @@ import Route from "./routes/route.js";
 
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({extended: true}));
 app.use('/', Route);
 
 const PORT = 8000;
