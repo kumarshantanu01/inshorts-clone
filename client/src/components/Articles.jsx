5a5412744d6ab6fa4181e234fd3e82fdea170854
kumarshantanu01 <shantanuk280@gmail.com>
2022-01-19 02:08:05 +0530

    feat: displaying news data

--- client/src/components/Articles.jsx
+++ client/src/components/Articles.jsx
@@ -1,10 +1,28 @@
-
+import { useEffect, useState } from "react";
+import { getNews } from "../service/api";
+import Article from "./Article";
 
 const Articles = () => {
+
+    const [news, setNews] = useState([]);
+
+    useEffect(() =>{
+        dailyNews();
+    }, [])
+
+    const dailyNews = async () =>{
+        let response = await getNews();
+        setNews(response.data);
+        console.log(response);
+    }
+
+
     return (
-        <>
-            Hello
-        </>
+        
+            news?.length > 0 && news.map(article =>(
+                <Article article={article}/>
+    ))
+        
     )
 }
 
